2ca29a4a5d528af01759a2b9b59389d8ad2399f6
girija321 <130287289+girija321@users.noreply.github.com>
2023-12-21 21:29:38 -0800

    Update RunnerClass.java

--- src/test/java/TestRunner/RunnerClass.java
+++ src/test/java/TestRunner/RunnerClass.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"src/test/resources/Feature"}, glue={"StepDefinations", "ApplicationHooks"}, tags="@smoke",
+@CucumberOptions(features={"src/test/resources/Feature"}, glue={"StepDefinations", "ApplicationHooks"}, //tags="@smoke",
             
                 dryRun=false,
                // monochrome=true,
